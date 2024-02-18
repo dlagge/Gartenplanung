f68cea07ada17ff24eb01e7455a43d820d2a399e
Dominique Lagger <dominique.lagger@students.fhnw.ch>
2024-02-18 22:27:16 +0100

    Kamera anders positioniert

--- projectfiles/init/camera.js
+++ projectfiles/init/camera.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 export class Camera {
     constructor() {
         this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-        this.camera.position.set(100,100,100);
+        this.camera.position.set(100,100,-100);
         this.camera.lookAt(0, 0, 0);
     }
 
