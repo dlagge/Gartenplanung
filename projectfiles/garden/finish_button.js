84c6e3b3ca432cccd66c5f60956996ad0c60f470
Dominique Lagger <dominique.lagger@students.fhnw.ch>
2024-03-24 12:30:39 +0100

    added mysql

---
+++ projectfiles/garden/finish_button.js
@@ -0,0 +1,52 @@
+import { createmodel, createmesh, getObjects, saveScene } from '../../main.js';
+import { Model } from './model.js';
+
+export class FinishButton {
+    constructor() {
+        this.createButton();
+    }
+
+    createButton() {
+        this.finishButton = document.createElement('button');
+        this.finishButton.setAttribute("id", "finishButton");
+        this.finishButton.style.position = 'absolute';
+        this.finishButton.style.left = '50%';
+        this.finishButton.style.top = '5rem';
+        this.finishButton.style.height = '5.5rem';
+        this.finishButton.style.display = 'none';
+        this.finishButton.style.borderRadius = '1rem';
+        this.finishButton.style.borderWidth = '0.35rem';
+        this.finishButton.style.borderColor = 'white';
+        this.finishButton.style.borderStyle = 'solid';
+        this.finishButton.style.background = '#b3847a';
+        this.finishButton.style.cursor = "pointer";
+        this.finishButton.style.color = '#ffffff';
+        this.finishButton.style.fontSize = '1.8rem';
+        this.finishButton.style.padding = '1rem';
+        this.finishButton.innerHTML = 'Speichern';
+        document.body.appendChild(this.finishButton);
+        this.finishButton.onmouseover = function () {
+            this.style.background = '#b4dbff';
+            this.style.transition = '1s';
+        };
+        this.finishButton.onmouseout = function () {
+            this.style.background = '#b3847a';
+        };
+        this.finishButton.onclick = function () {
+            let model = new Model();
+            model.setModelName('../../models/empty.glb');
+            model.getModel().load(model.getModelName(), (gltf) => {
+                let mesh = gltf.scene;
+                createmesh(mesh);
+            });
+            createmodel('./models/empty.glb');
+
+            this.style.display = 'none';
+            saveScene();
+        };
+    }
+
+    getButton() {
+        return this.finishButton;
+    }
+}
